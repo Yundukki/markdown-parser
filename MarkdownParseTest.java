31cad53082a1341397afa87b9a6bf5393fda4ebc
Yundukki <lch001@ucsd.edu>
2022-04-21 15:13:32 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 //imports junit
 
 public class MarkdownParseTest { //creates MarkdownParse test class
@@ -7,4 +11,12 @@ public class MarkdownParseTest { //creates MarkdownParse test class
     public void addition() { //names the method of test
         assertEquals(2, 1 + 1); //asserts 2 and 1+1 are equal
     }
+
+    @Test
+    public void GetLink() throws IOException{
+        assertEquals(List.of("https://something.com", "some-thing.html")
+        ,MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
+    }
+        
+    
 }
