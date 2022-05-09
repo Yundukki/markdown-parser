e58ce0ebf07bb0cefec602ab22d4c5097b1414ba
Yundukki <lch001@ucsd.edu>
2022-05-08 17:04:12 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,10 +7,7 @@ import java.util.List;
 //imports junit
 
 public class MarkdownParseTest { //creates MarkdownParse test class
-    @Test //calls a junit test
-    public void addition() { //names the method of test
-        assertEquals(2, 1+1); //asserts 2 and 1+1 are equal
-    }
+    
 
     @Test
     public void GetLink() throws IOException{
